e27e781e2868fcb9ecf19dc21e7121de7ae38979
Daniel Moro <daniel.moro@me.com>
2025-04-27 06:57:53 +0200

    update documentation scripts

--- docs.sh
+++
@@ -1,7 +0,0 @@
-swift package \
-    --allow-writing-to-directory ./docs \
-    generate-documentation --target SharedFoundation \
-    --disable-indexing \
-    --transform-for-static-hosting \
-    --hosting-base-path shared-foundation \
-    --output-path ./docs
